28c26e77516c075629bd4cb81d0ea2fc17633323
Miguel Peixe <miguel.peixe@automattic.com>
2025-09-18 09:43:02 -0300

    feat(my-account): subscription switch modal (#4177)

--- includes/plugins/woocommerce-subscriptions/class-subscriptions-tiers.php
+++ includes/plugins/woocommerce-subscriptions/class-subscriptions-tiers.php
@@ -14,6 +14,76 @@ defined( 'ABSPATH' ) || exit;
  */
 class Subscriptions_Tiers {
 	/**
+	 * Switch subscription links rendered in the page.
+	 *
+	 * @var array
+	 */
+	private static $switch_subscription_links = [];
+
+	/**
+	 * Initialize hooks.
+	 */
+	public static function init_hooks() {
+		add_filter( 'woocommerce_subscriptions_switch_link_text', [ __CLASS__, 'switch_link_text' ] );
+		add_filter( 'woocommerce_subscriptions_switch_link_text', [ __CLASS__, 'cache_switch_subscription_link_data' ], 10, 4 );
+		add_action( 'wp_footer', [ __CLASS__, 'print_switch_subscription_modal' ] );
+
+		// Order button text.
+		add_filter( 'wcs_place_subscription_order_text', [ __CLASS__, 'order_button_text' ], 9 );
+		add_filter( 'woocommerce_order_button_text', [ __CLASS__, 'order_button_text' ], 20 );
+		add_filter( 'option_woocommerce_subscriptions_order_button_text', [ __CLASS__, 'order_button_text' ], 9 );
+	}
+
+	/**
+	 * Switch link text.
+	 *
+	 * @return string The text of the switch subscription link.
+	 */
+	public static function switch_link_text() {
+		return __( 'Change Subscription', 'newspack-plugin' );
+	}
+
+	/**
+	 * Store switch subscription links in memory so we can render the modal later.
+	 *
+	 * @param string           $text         The text of the switch subscription link.
+	 * @param int              $item_id      The ID of the item.
+	 * @param array            $item         The order line item data.
+	 * @param \WC_Subscription $subscription The subscription.
+	 *
+	 * @return string The text of the switch subscription link.
+	 */
+	public static function cache_switch_subscription_link_data( $text, $item_id, $item, $subscription ) {
+		self::$switch_subscription_links[ $item_id ] = [
+			'item_id'      => $item_id,
+			'item'         => $item,
+			'subscription' => $subscription,
+		];
+		return $text;
+	}
+
+	/**
+	 * Modal switch subscription template.
+	 */
+	public static function print_switch_subscription_modal() {
+		if ( empty( self::$switch_subscription_links ) ) {
+			return;
+		}
+		foreach ( self::$switch_subscription_links as $data ) {
+			$product = wc_get_product( $data['item']['product_id'] );
+			if ( ! $product ) {
+				continue;
+			}
+			$parent_products = \WC_Subscriptions_Product::get_visible_grouped_parent_product_ids( $product );
+			if ( empty( $parent_products ) ) {
+				continue;
+			}
+			$product = wc_get_product( reset( $parent_products ) );
+			self::render_modal( $product, __( 'Change Subscription', 'newspack-plugin' ), __( 'Change Subscription', 'newspack-plugin' ), $data );
+		}
+	}
+
+	/**
 	 * Get the frequency of a product.
 	 *
 	 * @param \WC_Product $product Product object.
@@ -120,6 +190,28 @@ class Subscriptions_Tiers {
 	}
 
 	/**
+	 * Get product title.
+	 *
+	 * @param \WC_Product $product                   Product.
+	 * @param bool        $show_variation_attributes Whether the product title should include the variation attributes.
+	 *
+	 * @return string Product title.
+	 */
+	private static function get_product_title( $product, $show_variation_attributes = false ) {
+		$product_name = $product->get_title();
+		if ( $product->is_type( 'variation' ) ) {
+			if ( $show_variation_attributes ) {
+				$product_name = sprintf(
+					'%s (%s)',
+					$product_name,
+					implode( ', ', $product->get_variation_attributes() )
+				);
+			}
+		}
+		return $product_name;
+	}
+
+	/**
 	 * Render a subscription product card.
 	 *
 	 * @param \WC_Product $product                   Product.
@@ -139,37 +231,86 @@ class Subscriptions_Tiers {
 		} else {
 			$price = $product->get_price_html();
 		}
-		$product_name = $product->get_title();
-		if ( $product->is_type( 'variation' ) ) {
-			if ( $show_variation_attributes ) {
-				$product_name = sprintf(
-					'%s (%s)',
-					$product_name,
-					implode( ', ', $product->get_variation_attributes() )
-				);
-			}
-		}
 
 		?>
-		<label class="newspack-ui__input-card">
+		<label class="newspack-ui__input-card <?php echo $current ? esc_attr( 'current' ) : ''; ?>">
 			<?php if ( $current ) : ?>
 				<span class="newspack-ui__badge newspack-ui__badge--primary"><?php _e( 'Current', 'newspack-plugin' ); ?></span>
 			<?php endif; ?>
 			<input type="radio" name="product_id" value="<?php echo esc_attr( $product->get_id() ); ?>" <?php echo esc_attr( $selected ? 'checked' : '' ); ?>>
-			<strong><?php echo esc_html( $product_name ); ?></strong>
+			<strong><?php echo esc_html( self::get_product_title( $product, $show_variation_attributes ) ); ?></strong>
 			<span class="newspack-ui__helper-text"><?php echo $price; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></span>
 		</label>
 		<?php
 	}
 
 	/**
+	 * Render existing subscription info.
+	 *
+	 * @param \WC_Product      $product      Product.
+	 * @param \WC_Subscription $subscription Subscription.
+	 */
+	public static function render_existing_subscription_info( $product, $subscription ) {
+		?>
+		<div class="newspack-ui__notice newspack-ui__notice--warning">
+			<span class="newspack-ui__notice__content">
+				<?php
+				printf(
+					/* translators: %s: subscription product name */
+					esc_html__( 'You already have %s active.', 'newspack-plugin' ),
+					wp_kses_post( '<strong>' . self::get_product_title( $product, true ) . '</strong>' )
+				);
+				?>
+			</span>
+		</div>
+		<a class="newspack-ui__button newspack-ui__button--primary newspack-ui__button--wide" href="<?php echo esc_url( $subscription->get_view_order_url() ); ?>" aria-label="<?php esc_attr_e( 'View Subscription', 'newspack-plugin' ); ?>">
+			<?php esc_html_e( 'View', 'newspack-plugin' ); ?>
+		</a>
+		<?php
+	}
+
+	/**
+	 * Render frequency form control.
+	 *
+	 * Up until 3 frequencies, we render buttons.
+	 * After that, we render a select control.
+	 *
+	 * @param array  $frequencies       Frequencies.
+	 * @param string $current_frequency Current frequency.
+	 */
+	public static function render_frequency_control( $frequencies, $current_frequency ) {
+		if ( count( $frequencies ) <= 3 ) :
+			?>
+			<div class="newspack-ui__segmented-control__tabs">
+				<?php foreach ( $frequencies as $frequency ) : ?>
+					<button type="button" class="newspack-ui__button newspack-ui__button--small <?php echo esc_attr( $frequency === $current_frequency ? 'selected' : '' ); ?>">
+						<?php echo esc_html( WooCommerce_Subscriptions::get_frequency_label( $frequency ) ); ?>
+					</button>
+				<?php endforeach; ?>
+			</div>
+		<?php else : ?>
+			<div class="newspack-ui__segmented-control__tabs">
+				<select class="newspack-ui__button newspack-ui__button--small">
+					<?php foreach ( $frequencies as $i => $frequency ) : ?>
+						<option value="<?php echo esc_attr( $i ); ?>" <?php selected( $frequencies[ $i ], $current_frequency ); ?>>
+							<?php echo esc_html( WooCommerce_Subscriptions::get_frequency_label( $frequency ) ); ?>
+						</option>
+					<?php endforeach; ?>
+				</select>
+			</div>
+			<?php
+		endif;
+	}
+
+	/**
 	 * Render subscription tiers form.
 	 *
-	 * @param \WC_Product $product      Optional product.
-	 * @param string|null $title        Optional title.
-	 * @param string|null $button_label Optional button label.
+	 * @param \WC_Product $product             Optional product.
+	 * @param string|null $title               Optional title.
+	 * @param string|null $button_label        Optional button label.
+	 * @param array|null  $switch_subscription Switch subscription data or null.
 	 */
-	public static function render_form( $product = null, $title = null, $button_label = null ) {
+	public static function render_form( $product = null, $title = null, $button_label = null, $switch_subscription = null ) {
 		$tiers = self::get_tiers_by_frequency( $product );
 		if ( empty( $tiers ) ) {
 			return;
@@ -185,15 +326,22 @@ class Subscriptions_Tiers {
 			true
 		);
 
-		$frequencies = array_keys( $tiers );
+		$frequencies       = array_keys( $tiers );
 		$current_frequency = null;
 		$current_product   = null;
-		foreach ( $frequencies as $frequency ) {
-			foreach ( $tiers[ $frequency ] as $product ) {
-				if ( wcs_user_has_subscription( get_current_user_id(), $product->get_id(), 'active' ) ) {
-					$current_frequency = $frequency;
-					$current_product   = $product;
-					break 2;
+		$user_subscription = null;
+		if ( is_user_logged_in() ) {
+			$user_subscriptions = wcs_get_users_subscriptions( get_current_user_id() );
+			foreach ( $frequencies as $frequency ) {
+				foreach ( $tiers[ $frequency ] as $product ) {
+					foreach ( $user_subscriptions as $subscription ) {
+						if ( $subscription->has_product( $product->get_id() ) && $subscription->has_status( 'active' ) ) {
+							$current_frequency = $frequency;
+							$current_product   = $product;
+							$user_subscription = $subscription;
+							break 2;
+						}
+					}
 				}
 			}
 		}
@@ -211,17 +359,22 @@ class Subscriptions_Tiers {
 		$title        = $title ?? __( 'Complete your transaction', 'newspack-plugin' );
 		$button_label = $button_label ?? __( 'Purchase', 'newspack-plugin' );
 
+		// If the user has an active subscription and this is not a switch, render
+		// the existing subscription info instead of the tiers form.
+		if ( $user_subscription && empty( $switch_subscription ) ) {
+			self::render_existing_subscription_info( $current_product, $user_subscription );
+			return;
+		}
+
 		?>
 		<form class="newspack__subscription-tiers__form" target="newspack_modal_checkout_iframe" data-title="<?php echo esc_attr( $title ); ?>">
 			<?php if ( ! $is_single_tier ) : ?>
 				<div class="newspack-ui__segmented-control">
-					<?php if ( count( $frequencies ) > 1 ) : ?>
-						<div class="newspack-ui__segmented-control__tabs">
-							<?php foreach ( $frequencies as $frequency ) : ?>
-								<button type="button" class="newspack-ui__button newspack-ui__button--small <?php echo esc_attr( $frequency === $current_frequency ? 'selected' : '' ); ?>"><?php echo esc_html( WooCommerce_Subscriptions::get_frequency_label( $frequency ) ); ?></button>
-							<?php endforeach; ?>
-						</div>
-					<?php endif; ?>
+					<?php
+					if ( count( $frequencies ) > 1 ) {
+						self::render_frequency_control( $frequencies, $current_frequency );
+					}
+					?>
 					<div class="newspack-ui__segmented-control__content">
 						<?php foreach ( $tiers as $frequency => $products ) : ?>
 							<div class="newspack-ui__segmented-control__panel">
@@ -246,6 +399,10 @@ class Subscriptions_Tiers {
 			?>
 			<input type="hidden" name="newspack_checkout" value="1">
 			<input type="hidden" name="modal_checkout" value="1">
+			<?php if ( ! empty( $switch_subscription ) ) : ?>
+				<input type="hidden" name="switch-subscription" value="<?php echo esc_attr( $switch_subscription['subscription']->get_id() ); ?>">
+				<input type="hidden" name="item" value="<?php echo absint( $switch_subscription['item_id'] ); ?>">
+			<?php endif; ?>
 
 			<button type="submit" class="newspack-ui__button newspack-ui__button--primary newspack-ui__button--wide"><?php echo esc_html( $button_label ); ?></button>
 			<button type="button" class="newspack-ui__button newspack-ui__button--ghost newspack-ui__button--wide newspack-ui__modal__cancel"><?php _e( 'Cancel', 'newspack-plugin' ); ?></button>
@@ -260,13 +417,14 @@ class Subscriptions_Tiers {
 	 * If no grouped or variable subscription product is provided,
 	 * all non-donation subscription products are rendered.
 	 *
-	 * @param \WC_Product|null $product      Optional product.
-	 * @param string|null      $title        Optional title.
-	 * @param string|null      $button_label Optional button label.
+	 * @param \WC_Product|null $product             Optional product.
+	 * @param string|null      $title               Optional title.
+	 * @param string|null      $button_label        Optional button label.
+	 * @param array|null       $switch_subscription Switch subscription data or null.
 	 */
-	public static function render_modal( $product = null, $title = null, $button_label = null ) {
+	public static function render_modal( $product = null, $title = null, $button_label = null, $switch_subscription = null ) {
 		?>
-		<div class="newspack-ui newspack-ui__modal-container newspack__subscription-tiers" data-state="closed" data-product-id="<?php echo esc_attr( $product ? $product->get_id() : '' ); ?>">
+		<div class="newspack-ui newspack-ui__modal-container newspack__subscription-tiers" data-state="closed" data-product-id="<?php echo esc_attr( $product ? $product->get_id() : '' ); ?>" data-subscription-id="<?php echo esc_attr( $switch_subscription ? $switch_subscription['subscription']->get_id() : '' ); ?>">
 			<div class="newspack-ui__modal-container__overlay"></div>
 			<div class="newspack-ui__modal newspack-ui__modal--small">
 				<header class="newspack-ui__modal__header">
@@ -277,10 +435,25 @@ class Subscriptions_Tiers {
 					</button>
 				</header>
 				<div class="newspack-ui__modal__content">
-					<?php self::render_form( $product, $title, $button_label ); ?>
+					<?php self::render_form( $product, $title, $button_label, $switch_subscription ); ?>
 				</div>
 			</div>
 		</div>
 		<?php
 	}
+
+	/**
+	 * Order button text.
+	 *
+	 * @param string $text The text of the order button.
+	 *
+	 * @return string The text of the order button.
+	 */
+	public static function order_button_text( $text ) {
+		if ( method_exists( 'WC_Subscriptions_Switcher', 'cart_contains_switches' ) && \WC_Subscriptions_Switcher::cart_contains_switches( 'any' ) ) {
+			return __( 'Change Subscription', 'newspack-plugin' );
+		}
+		return $text;
+	}
 }
+Subscriptions_Tiers::init_hooks();
--- src/reader-activation/subscription-tiers-form.js
+++ src/reader-activation/subscription-tiers-form.js
@@ -11,6 +11,27 @@ export default function init() {
 			const modal = form.closest( '.newspack-ui__modal-container' );
 			const cancelButton = form.querySelector( '.newspack-ui__modal__cancel' );
 
+			let isFormValid = false;
+
+			const validateForm = () => {
+				const selected = form.querySelector( '.current input[type="radio"]:checked' );
+				if ( selected ) {
+					form.querySelector( 'button[type="submit"]' ).disabled = true;
+					isFormValid = false;
+				} else {
+					form.querySelector( 'button[type="submit"]' ).disabled = false;
+					isFormValid = true;
+				}
+			};
+
+			// Watch radio input selection changes and disable the submit button if selecting the "current" option.
+			const radioInputs = form.querySelectorAll( 'input[type="radio"]' );
+			radioInputs.forEach( radio => {
+				radio.addEventListener( 'input', validateForm );
+			} );
+
+			validateForm();
+
 			if ( modal ) {
 				cancelButton.addEventListener( 'click', () => {
 					modal?.setAttribute( 'data-state', 'closed' );
@@ -20,24 +41,35 @@ export default function init() {
 			}
 
 			form.addEventListener( 'submit', ev => {
+				if ( ! isFormValid ) {
+					ev.preventDefault();
+					return;
+				}
+
 				// Bail if this is a variation modal from the Checkout Button block,
 				// as it has its own form submission logic.
 				if ( modal?.classList.contains( 'newspack-blocks__modal-variation' ) ) {
 					return;
 				}
+				// Bail if the modal checkout API is not available.
 				if ( ! window.newspackOpenModalCheckout ) {
 					return;
 				}
 				ev.preventDefault();
+				let completed = false;
 				const formData = new FormData( form );
 				const params = new URLSearchParams( formData );
 				modal?.setAttribute( 'data-state', 'closed' );
 				window.newspackOpenModalCheckout( {
 					url: form.action + '?' + params.toString(),
 					title: form.dataset.title,
-					actionType: form.dataset.actionType || undefined,
+					onCheckoutComplete: () => {
+						completed = true;
+					},
 					onClose: () => {
-						modal?.setAttribute( 'data-state', 'open' );
+						if ( completed ) {
+							window.location.reload();
+						}
 					},
 				} );
 			} );
