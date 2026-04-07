85aa560f72c022f864b5be497e4eca68f5ec4da5
Derrick Koo <dkoo@users.noreply.github.com>
2026-04-07 09:53:16 -0600

    feat(access-control): advanced settings for RSS content restriction (#4613)
    * feat(access-control): advanced settings
    
    * refactor: use render_block for better performance
    
    * chore: re-word Everlit option for clarity
    
    * fix: also remove everlit embeds from front-end metered posts
    
    * fix: copilot feedback
    
    * fix: modal button layout, institutions nav, AdvancedSettingsConfig type
    
    * chore: remove everlit handling (handled by Everlit plugin)
    
    * chore: remove ALL the everlit
    
    * refactor: rename Content_Gate_Settings to Content_Gate_Global_Settings, for clarity
    
    * fix: discard local state if closing modal without saving
    
    * fix: missing files
    
    * fix: reset advanced settings data to store on open modal
    
    * refactor: rename `Content_Gate_Global_Settings` to `Content_Gate_Advanced_Settings`
    
    ---------
    
    Co-authored-by: Thomas Guillot <thomas@automattic.com>

--- src/wizards/audience/views/content-gates/advanced-settings.tsx
+++ src/wizards/audience/views/content-gates/advanced-settings.tsx
@@ -17,18 +17,14 @@ import { Button, Modal } from '../../../../../packages/components/src';
 import { useWizardData } from '../../../../../packages/components/src/wizard/store/utils';
 import { WIZARD_STORE_NAMESPACE } from '../../../../../packages/components/src/wizard/store';
 import { useWizardApiFetch } from '../../../hooks/use-wizard-api-fetch';
-import { PREMIUM_NEWSLETTERS_WIZARD_SLUG } from './consts';
-
-type PremiumNewslettersConfig = {
-	auto_signup: boolean;
-};
+import { AUDIENCE_CONTENT_GATES_WIZARD_SLUG } from './consts';
 
 const AdvancedSettings = ( { closeModal, showModal }: { closeModal: () => void; showModal: boolean } ) => {
-	const wizardData = useWizardData( PREMIUM_NEWSLETTERS_WIZARD_SLUG ) as WizardData;
-	const initialConfig = { ...( ( wizardData?.config as PremiumNewslettersConfig ) || { auto_signup: true } ) };
-	const { wizardApiFetch, isFetching, resetError, setError } = useWizardApiFetch( PREMIUM_NEWSLETTERS_WIZARD_SLUG );
+	const wizardData = useWizardData( AUDIENCE_CONTENT_GATES_WIZARD_SLUG ) as WizardData;
+	const initialConfig = { ...( wizardData?.config?.advanced_settings || {} ) };
+	const { wizardApiFetch, isFetching, resetError, setError } = useWizardApiFetch( AUDIENCE_CONTENT_GATES_WIZARD_SLUG );
 	const { addNotice, resetNotices, updateWizardSettings } = useDispatch( WIZARD_STORE_NAMESPACE );
-	const [ config, setConfig ] = useState< PremiumNewslettersConfig >( initialConfig );
+	const [ config, setConfig ] = useState< AdvancedSettingsConfig >( initialConfig );
 
 	useEffect( () => {
 		if ( showModal ) {
@@ -36,33 +32,33 @@ const AdvancedSettings = ( { closeModal, showModal }: { closeModal: () => void; 
 		}
 	}, [ showModal ] );
 
-	const updateConfig = useRef< ( _config: PremiumNewslettersConfig ) => void >();
-	const handleUpdateConfig = ( _config: PremiumNewslettersConfig ) => {
+	const updateConfig = useRef< ( _config: AdvancedSettingsConfig ) => void >();
+	const handleUpdateConfig = ( _config: AdvancedSettingsConfig ) => {
 		if ( isFetching ) {
 			return;
 		}
 		resetError();
 		resetNotices();
-		wizardApiFetch< Gate >(
+		wizardApiFetch< AdvancedSettingsConfig >(
 			{
-				path: `/newspack/v1/wizard/${ PREMIUM_NEWSLETTERS_WIZARD_SLUG }/config`,
+				path: `/newspack/v1/wizard/${ AUDIENCE_CONTENT_GATES_WIZARD_SLUG }/settings`,
 				method: 'POST',
 				data: {
-					config: _config,
+					advanced_settings: _config,
 				},
 			},
 			{
-				onSuccess: () => {
+				onSuccess: ( data: AdvancedSettingsConfig ) => {
 					setConfig( _config );
 					updateWizardSettings( {
-						slug: PREMIUM_NEWSLETTERS_WIZARD_SLUG,
-						path: [ 'config' ],
-						value: _config,
+						slug: AUDIENCE_CONTENT_GATES_WIZARD_SLUG,
+						path: [ 'config', 'advanced_settings' ],
+						value: data,
 					} );
 					addNotice( {
 						message: __( 'Settings updated.', 'newspack-plugin' ),
 						type: 'success',
-						id: 'premium-newsletters-advanced-settings-updated',
+						id: 'content-gates-advanced-settings-updated',
 						actions: [ { label: __( 'Undo', 'newspack-plugin' ), onClick: () => updateConfig.current?.( initialConfig ) } ],
 					} );
 				},
@@ -75,31 +71,32 @@ const AdvancedSettings = ( { closeModal, showModal }: { closeModal: () => void; 
 			}
 		);
 	};
+
 	updateConfig.current = handleUpdateConfig;
 	return (
 		showModal && (
 			<Modal onClose={ closeModal } size="medium" title={ __( 'Advanced settings', 'newspack-plugin' ) } onRequestClose={ closeModal }>
 				<VStack>
 					<ToggleControl
-						label={ __( 'Auto signup', 'newspack-plugin' ) }
-						help={ __( 'Automatically sign up users when they meet access requirements for premium newsletters.', 'newspack-plugin' ) }
-						checked={ config?.auto_signup }
-						onChange={ value => setConfig( { ...config, auto_signup: value } ) }
+						label={ __( 'Restrict content in feeds', 'newspack-plugin' ) }
+						help={ __( 'Truncate restricted content in RSS feeds.', 'newspack-plugin' ) }
+						checked={ config?.restrict_feeds }
+						onChange={ value => setConfig( { ...config, restrict_feeds: value } ) }
 					/>
+					<HStack justify="end">
+						<Button variant="tertiary" disabled={ isFetching } onClick={ closeModal }>
+							{ __( 'Cancel', 'newspack-plugin' ) }
+						</Button>
+						<Button
+							variant="primary"
+							disabled={ isFetching || JSON.stringify( wizardData?.config?.advanced_settings || {} ) === JSON.stringify( config ) }
+							loading={ isFetching }
+							onClick={ () => updateConfig.current?.( config ) }
+						>
+							{ __( 'Save', 'newspack-plugin' ) }
+						</Button>
+					</HStack>
 				</VStack>
-				<HStack justify="end">
-					<Button variant="tertiary" disabled={ isFetching } onClick={ closeModal }>
-						{ __( 'Cancel', 'newspack-plugin' ) }
-					</Button>
-					<Button
-						variant="primary"
-						disabled={ isFetching || JSON.stringify( wizardData?.config || {} ) === JSON.stringify( config ) }
-						loading={ isFetching }
-						onClick={ () => updateConfig.current?.( config ) }
-					>
-						{ __( 'Save', 'newspack-plugin' ) }
-					</Button>
-				</HStack>
 			</Modal>
 		)
 	);
